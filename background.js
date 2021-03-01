f26c49c912f90c35981020055a7b7031be867ab9
Amirhossein <amir.h.nouri2000@gmail.com>
2021-03-01 22:11:14 +0330

    :heavy_plus_sign: Add chrome crx to install easier

--- background.js
+++ background.js
@@ -1,4 +1,15 @@
-// Initial intervalId value
 chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.sync.set({ intervalId: null }, function () {});
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "meet.google.com" },
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
+    ]);
+  });
 });
