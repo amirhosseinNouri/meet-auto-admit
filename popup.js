ce91ea9039c318d559dab6914023931da3a7873d
Amirhossein <amir.h.nouri2000@gmail.com>
2021-03-01 10:47:02 +0330

    :heavy_plus_sign: Add auto admit functionality

--- popup.js
+++ popup.js
@@ -1,6 +1,36 @@
-let inter = setInterval(() => {
-    let elems = [...document.getElementsByClassName("CwaK9")]
+let intervalId = undefined;
+const start = document.querySelector("#start");
+const accept = () => {
+  intervalId = setInterval(() => {
+    let elems = [...document.querySelectorAll(".CwaK9")];
     if (elems.length > 0) {
-        elems[1].click();
+      elems[1].click();
     }
-}, 1000);
+  }, 1000);
+};
+
+start.addEventListener("click", () => {
+  if (!intervalId) {
+    start.style.backgroundColor = "red";
+    accept();
+
+    start.innerText = "stop";
+  } else {
+    start.style.backgroundColor = "teal";
+    clearInterval(intervalId);
+    intervalId = undefined;
+    start.innerText = "start";
+  }
+});
+
+start.addEventListener("click", () => {
+  chrome.tabs.executeScript(
+    {
+      code: "(" + accept + ")();",
+    },
+    () => {
+      if (!intervalId) alert("auto admit is off");
+      else alert("auto admti is on");
+    }
+  );
+});
