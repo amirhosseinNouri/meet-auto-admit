a49ae85d1e16e3ebf5097d3a948d405d6c619130
Amirhossein <amir.h.nouri2000@gmail.com>
2021-03-01 20:33:25 +0330

    :green_heart: Remove unnecessary permissions

--- background.js
+++ background.js
@@ -1,16 +1,3 @@
 chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.sync.set({ intervalId: null }, function () {});
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: "meet.google.com" },
-          }),
-        ],
-        actions: [new chrome.declarativeContent.ShowPageAction()],
-      },
-    ]);
-  });
 });
-
