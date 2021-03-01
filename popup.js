af6804eafaba108c8e4c1cd980e5b372492ef1fe
Amirhossein <amir.h.nouri2000@gmail.com>
2021-03-01 14:48:17 +0330

    :green_heart: Save intervalId inside localstorage

--- popup.js
+++ popup.js
@@ -1,42 +1,51 @@
 const red = "#DA3F2C";
 const green = "#2C645B";
 
-let intervalId = undefined;
-const start = document.querySelector("#start");
-
-const accept = () => {
-  intervalId = setInterval(() => {
-    let elems = [...document.querySelectorAll(".CwaK9")];
-    if (elems.length > 0) {
-      elems[1].click();
-    }
-  }, 1000);
+let intervalId;
+const restoreId = () => {
+  intervalId = JSON.parse(localStorage.getItem("intervalId"));
 };
 
-start.addEventListener("click", () => {
+const saveId = () => {
+  localStorage.setItem("intervalId", JSON.stringify(intervalId));
+};
+
+const accept = () => {
+  restoreId();
+
   if (!intervalId) {
-    start.style.backgroundColor = red;
-    document.body.style.border = "3px solid " + red;
-    accept();
-
-    start.innerText = "stop";
+    intervalId = setInterval(() => {
+      let elems = [...document.querySelectorAll(".CwaK9")];
+      if (elems.length > 0) {
+        elems[1].click();
+      }
+    }, 1000);
   } else {
-    start.style.backgroundColor = green;
-    document.body.style.border = "3px solid " + green;
     clearInterval(intervalId);
-    intervalId = undefined;
-    start.innerText = "start";
+    intervalId = null;
   }
-});
+  saveId();
+  return intervalId;
+};
 
+const start = document.querySelector("#start");
 start.addEventListener("click", () => {
   chrome.tabs.executeScript(
     {
       code: "(" + accept + ")();",
     },
-    () => {
-      //   if (!intervalId) alert("auto admit is off");
-      //   else alert("auto admti is on");
+    (result) => {
+      if (result == "") {
+        start.style.backgroundColor = red;
+        document.body.style.border = "3px solid " + green;
+        start.innerText = "stop";
+      } else {
+        start.style.backgroundColor = green;
+        document.body.style.border = "3px solid " + red;
+        start.innerText = "start";
+      }
     }
   );
 });
+
+restoreId();
